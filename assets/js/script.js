535e5ac3f92fd42ba6b856169a8a9744de8b82f1
Donovan Maidens <donovan@anomalous.co.za>
2016-08-26 15:39:22 +0200

    First Push For Bootpresswp child theme

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Bootpresswp Child Theme
+// Theme Designed and Scripted by Anomalous 
+// http://www.anomalous.co.za
+// 05 September 2015
+
+jQuery(document).ready(function() {
+	
+});
+
+// Last Line of Code
